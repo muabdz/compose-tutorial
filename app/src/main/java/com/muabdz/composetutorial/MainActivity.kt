8e539d063a0d8b7763f110f441f42e56d970be97
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2023-10-30 23:05:54 +0700

    add the middle part

--- app/src/main/java/com/muabdz/composetutorial/MainActivity.kt
+++ app/src/main/java/com/muabdz/composetutorial/MainActivity.kt
@@ -3,13 +3,28 @@ package com.muabdz.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.muabdz.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BusinessCard("Mu\'adz Abdurrahman","Android Developer")
                 }
             }
         }
@@ -30,17 +45,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BusinessCard(name: String, title: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        MiddlePart(name = name, title = title, modifier = modifier)
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MiddlePart(name: String, title: String, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(8.dp).fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            null,
+            modifier = Modifier.background(color = Color.Cyan)
+        )
+        Text(
+            text = name,
+            modifier = modifier,
+            textAlign = TextAlign.Center,
+            fontSize = 36.sp,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = title,
+            modifier = modifier,
+            textAlign = TextAlign.Center,
+            fontSize = 24.sp,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_2, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ComposeTutorialTheme {
-        Greeting("Android")
+        BusinessCard("Mu\'adz Abdurrahman","Android Developer")
     }
 }
